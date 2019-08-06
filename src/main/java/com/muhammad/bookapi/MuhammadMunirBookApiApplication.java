bb412359665f88e9da9456647439b5558308b09d
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-08-05 21:24:43 -0500

    complete book api

---
+++ src/main/java/com/muhammad/bookapi/MuhammadMunirBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.muhammad.bookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuhammadMunirBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuhammadMunirBookApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/muhammad/bookapi/services/BookService.java
@@ -0,0 +1,57 @@
+package com.muhammad.bookapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.muhammad.bookapi.modell.Book;
+import com.muhammad.bookapi.repositories.BookRepository;
+
+@Service
+public class BookService {
+
+	private final BookRepository bookRepository;
+
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+	// returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if(optionalBook.isPresent()) {
+			Book updatedBook = optionalBook.get();
+			updatedBook.setTitle(title);
+			updatedBook.setDescription(desc);
+			updatedBook.setNumberOfPages(numOfPages);
+			updatedBook.setLanguage(lang);
+           bookRepository.save(updatedBook);
+           return updatedBook;
+           } else {
+            return null;
+        }
+	}
+
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+	
+}
